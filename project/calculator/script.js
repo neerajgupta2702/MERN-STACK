1d71121b58f8e48c12b9a13dcea90709913834e4
neerajgupta2702 <120675286+neerajgupta2702@users.noreply.github.com>
2024-01-29 22:30:11 +0530

    Update script.js

--- project/calculator/script.js
+++ project/calculator/script.js
@@ -17,11 +17,8 @@ if(e.target.innerHTML=='='){
     string=string.substring(0, string.length-1);
     input.value=string;
 
-  } 
-  else if(e.target.innerHTML=='%'){
-    
-    
   }
+        
    else{
     string+=e.target.innerHTML;
     input.value=string;
